bdbaa211650872bbd9c64488b9a8db99cfe86e1a
Jien Huang <huangjien@gmail.com>
2015-12-02 14:10:02 +0000

    disable csrf, don't know why it does not work.

--- autoP/__init__.py
+++ autoP/__init__.py
@@ -1,14 +1,20 @@
 from flask import Flask
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
+from flask_wtf import CsrfProtect
+
 
 app = Flask(__name__)
-app.secret_key = 'need to fix it later'
-app.config['SECRET_KEY'] = 'need to fix it later'
-
+app.secret_key = 'needtofixitlater'
+app.config['SECRET_KEY'] = 'needtofixitlater'
+app.config['WTF_CSRF_ENABLED'] = False
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'login'
 # login_manager.id_attribute = 'email'
 login_manager.init_app(app)
+csrf = CsrfProtect()
+csrf.init_app(app)
+Bootstrap(app)
 
 import autoP.views
